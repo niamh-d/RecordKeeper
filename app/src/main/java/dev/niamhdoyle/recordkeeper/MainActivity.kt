cf4742d2810fef0659bddfc1137963b3e4fa3a8d
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-07-24 11:39:56 +0300

    running fragment

--- app/src/main/java/dev/niamhdoyle/recordkeeper/MainActivity.kt
+++ app/src/main/java/dev/niamhdoyle/recordkeeper/MainActivity.kt
@@ -2,10 +2,19 @@ package dev.niamhdoyle.recordkeeper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import dev.niamhdoyle.recordkeeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vb: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+
+        supportFragmentManager.commit { add(R.id.frame_content, RunningFragment())}
     }
 }
