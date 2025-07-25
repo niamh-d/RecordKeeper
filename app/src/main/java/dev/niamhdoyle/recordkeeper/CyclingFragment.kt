46651310fb3c87f9b010c6323b3b02c25246cd6a
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-07-25 12:32:06 +0300

    cycling records go to edit cycling records activity

--- app/src/main/java/dev/niamhdoyle/recordkeeper/CyclingFragment.kt
+++ app/src/main/java/dev/niamhdoyle/recordkeeper/CyclingFragment.kt
@@ -1,9 +1,11 @@
 package dev.niamhdoyle.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import dev.niamhdoyle.recordkeeper.databinding.FragmentCyclingBinding
 
@@ -18,5 +20,32 @@ class CyclingFragment : Fragment() {
     ): View {
         vb = FragmentCyclingBinding.inflate(inflater, container, false)
         return vb.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun getName(heading: TextView): String {
+        return heading.text.toString()
+    }
+
+    private fun setupClickListeners() {
+        vb.containerLongestRide.setOnClickListener {
+            launchRunningRecordScreen(getName(vb.textViewLongestRideHeading))
+        }
+        vb.containerBiggestClimb.setOnClickListener {
+            launchRunningRecordScreen(getName(vb.textViewBiggestClimbHeading))
+        }
+        vb.containerBestAvgSpeed.setOnClickListener {
+            launchRunningRecordScreen(getName(vb.textViewBestAvgSpeedHeading))
+        }
+    }
+
+    private fun launchRunningRecordScreen(recordName: String) {
+        val intent = Intent(context, EditRunningRecordActivity::class.java)
+        intent.putExtra("distance", recordName)
+        startActivity(intent)
     }
 }
