1a065568a7970e34b361a86d6527feee2aed80ea
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-07-29 01:17:24 +0300

    able to clear records

--- app/src/main/java/dev/niamhdoyle/recordkeeper/running/EditRunningRecordActivity.kt
+++ app/src/main/java/dev/niamhdoyle/recordkeeper/running/EditRunningRecordActivity.kt
@@ -10,26 +10,31 @@ import dev.niamhdoyle.recordkeeper.databinding.ActivityEditRunningRecordBinding
 class EditRunningRecordActivity : AppCompatActivity() {
 
     private lateinit var vb: ActivityEditRunningRecordBinding
-    private lateinit var runningPreferences: SharedPreferences
-    private val recordName: String? by lazy { intent.getStringExtra("distance") }
+    private val runningPreferences by lazy { getSharedPreferences("running", Context.MODE_PRIVATE) }
+    private val recordName by lazy { intent.getStringExtra("distance") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         vb = ActivityEditRunningRecordBinding.inflate(layoutInflater)
         setContentView(vb.root)
-
-        title = formatTitle()
-
+        setupUi()
         displayRecord()
+    }
+
+    private fun setupUi() {
+        title = formatTitle()
         vb.buttonSave.setOnClickListener {
             saveRecord()
+            finish()
+        }
+        vb.buttonClear.setOnClickListener {
+            clearRecord()
             finish()
         }
     }
 
     private fun displayRecord() {
-        runningPreferences = getSharedPreferences("running", Context.MODE_PRIVATE)
         vb.editTextRecord.setText(runningPreferences.getString("${recordName}_record", null))
         vb.editTextDate.setText(runningPreferences.getString("${recordName}_date", null))
     }
@@ -41,6 +46,13 @@ class EditRunningRecordActivity : AppCompatActivity() {
         runningPreferences.edit {
             putString("${recordName}_record", record)
             putString("${recordName}_date", date)
+        }
+    }
+
+    private fun clearRecord() {
+        runningPreferences.edit {
+            remove("${recordName}_record")
+            remove("${recordName}_date")
         }
     }
 
