6a007e10472ef476b8845a6f86a79d5986f40e19
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-07-29 16:51:03 +0300

    screendata data class

---
+++ app/src/main/java/dev/niamhdoyle/recordkeeper/editrecord/EditRecordActivity.kt
@@ -0,0 +1,70 @@
+package dev.niamhdoyle.recordkeeper.editrecord
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import dev.niamhdoyle.recordkeeper.databinding.ActivityEditRecordBinding
+import java.io.Serializable
+
+class EditRecordActivity : AppCompatActivity() {
+
+    private lateinit var vb: ActivityEditRecordBinding
+    private val screenData by lazy {
+        intent.getSerializableExtra("screen_data") as ScreenData
+    }
+    private val recordPreferences by lazy { getSharedPreferences(screenData.sharedPreferences, Context.MODE_PRIVATE) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        vb = ActivityEditRecordBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+        setupUi()
+        displayRecord()
+    }
+
+    private fun setupUi() {
+        title = formatTitle()
+        vb.buttonSave.setOnClickListener {
+            saveRecord()
+            finish()
+        }
+        vb.buttonClear.setOnClickListener {
+            clearRecord()
+            finish()
+        }
+    }
+
+    private fun displayRecord() {
+        vb.editTextRecord.setText(recordPreferences.getString("${screenData.record}_record", null))
+        vb.editTextDate.setText(recordPreferences.getString("${screenData.record}_date", null))
+    }
+
+    private fun saveRecord() {
+        val record = vb.editTextRecord.text.toString()
+        val date = vb.editTextDate.text.toString()
+
+        recordPreferences.edit {
+            putString("${screenData.record}_record", record)
+            putString("${screenData.record}_date", date)
+        }
+    }
+
+    private fun clearRecord() {
+        recordPreferences.edit {
+            remove("${screenData.record}_record")
+            remove("${screenData.record}_date")
+        }
+    }
+
+    private fun formatTitle(): String {
+        return "Edit ${screenData.record} record"
+    }
+
+    data class ScreenData (
+        val record: String,
+        val sharedPreferences: String,
+        val recordFieldHint: String
+    ) : Serializable
+}
